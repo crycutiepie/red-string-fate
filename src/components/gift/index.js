dc1bf510a33553724d0f05eb6f995b9b95d27f18
Cristian Barrientos Montoya <cristian_barrientos23132@elpoli.edu.co>
2017-03-09 02:41:51 -0500

    Initial react conf
    Added initial react config. cake and gift component were created too.

---
+++ src/components/gift/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Gift extends Component {
+  render(){
+    return (<div></div>);
+  }
+}
+
+export default Gift;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Landing from './components/landing';
+import Gift from './components/gift';
 import App from './App';
 import './index.css';
+import { Router, Route, Redirect, browserHistory } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Redirect from="/" to="/landing" />
+    <Route path="/" component={App}>
+      <Route path="landing" component={Landing}/>
+      <Route path="gift" component={Gift}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
